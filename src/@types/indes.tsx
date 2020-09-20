3a7a5eba05ebbf1d68ba3704921a5bbe8d6eea00
cristopher <cristopherrabelo@gmail.com>
2020-09-20 13:05:04 -0300

    Inclemento pastas
    O repo estava com informações estática, então assim como no profile puxei as informações

---
+++ src/@types/indes.tsx
@@ -0,0 +1,26 @@
+export interface APiUser{
+    login: string;
+    name: string;
+    followers: number;
+    following: number;
+    public_repos: number;
+    avar_url: string;
+    blog?: string;
+    company?: string;
+    email?: string;
+    location?: string;
+}
+
+export interface APIRepo{
+    name: string;
+    owner:{
+        login: string;
+    };
+    
+    stargazers_count: number;
+    forks: number;
+    html_url: string;
+    language?: string;
+    description?: string;
+    
+}
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Line, GitHubLogo } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+        <Line />
+        <GitHubLogo />
+    </Container>
+
+
+
+  );
+}
+
+export default Footer;
